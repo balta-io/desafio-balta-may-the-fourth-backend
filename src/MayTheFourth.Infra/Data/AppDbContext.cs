33f250854178b284d32c7329b2c4582456cb6a61
Igor Santiago <contato.igorsantiago@outlook.com>
2024-04-19 14:34:13 -0300

    Project basic structure

---
+++ src/MayTheFourth.Infra/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFourth.Infra.Data;
+
+public class AppDbContext : DbContext
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
