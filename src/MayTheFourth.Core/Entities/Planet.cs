42f181e488d314cb9e0346e7d9401c8ae3bdf1e3
Igor Santiago <contato.igorsantiago@outlook.com>
2024-04-20 17:08:02 -0300

    Add some unit test to planet handlers

--- src/MayTheFourth.Core/Entities/Planet.cs
+++ src/MayTheFourth.Core/Entities/Planet.cs
@@ -8,7 +8,7 @@ public class Planet : Entity
     public string Diameter { get; set; } = string.Empty;
     public string RotationPeriod { get; set; } = string.Empty;
     public string OrbitalPeriod { get; set; } = string.Empty;
-    public string Gravity { get; set; } = string.Empty;
+    public double Gravity { get; set; }
     public string Population { get; set; } = string.Empty;
     public string Climate { get; set; } = string.Empty;
     public string Terrain { get; set; } = string.Empty;
---
+++ src/MayTheFourth.Tests/Repositories/FakePlanetRepository.cs
@@ -0,0 +1,38 @@
+﻿using MayTheFourth.Core.Entities;
+using MayTheFourth.Core.Interfaces.Repositories;
+
+namespace MayTheFourth.Tests.Repositories;
+
+public class FakePlanetRepository : IPlanetRepository
+{
+    public List<Planet> planets = new()
+    {
+        new() { Name = "Alderaan", Gravity = 9.81 },
+        new() { Name = "Tatooine", Gravity = 9.81 },
+        new() { Name = "Naboo", Gravity = 9.81 },
+        new() { Name = "Coruscant", Gravity = 9.81 },
+        new() { Name = "Hoth", Gravity = 9.81 }
+    };
+
+    public Task<bool> AnyAsync(string name, string gravity)
+    {
+        if (string.Equals(name, planets[0].Name))
+            return Task.FromResult(true);
+
+        return Task.FromResult(false);
+    }
+
+    public async Task<List<Planet>?> GetAllPlanets()
+    {
+        await Task.Delay(1000);
+        return planets;
+    }
+
+    public Task SaveAsync(Planet planet, CancellationToken cancellationToken)
+    {
+        if (planet is null)
+            return Task.FromResult(false);
+
+        return Task.FromResult(true);
+    }
+}
