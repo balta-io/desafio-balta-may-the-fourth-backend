0fd85b41c9f827d8fed4fe5ed5727c901903a93f
Joao Kalin <vasconcelosjoao438@gmail.com>
2024-04-20 20:12:26 -0300

    setup: new backend configuration structure

---
+++ src/MayTheFourth.Backend/Configurations/ApiConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace MayTheFourth.Backend.Configurations
+{
+    public static class ApiConfiguration
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext(services, configuration);
+            services.AddSwaggerConfiguration();
+        }
+
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwaggerConfiguration(env);
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            //add context
+        }
+
+    }
+}
---
+++ src/MayTheFourth.Backend/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace MayTheFourth.Backend.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        private const string ApiTitle = "May The Fourth API";
+        private const int ApiVersion = 1;
+
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc($"v{ApiVersion}", new OpenApiInfo
+                {
+                    Title = ApiTitle,
+                    Version = $"v{ApiVersion}"
+                });
+            });
+        }
+
+        public static void UseSwaggerConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/v{ApiVersion}/swagger.json", ApiTitle);
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+        }
+    }
+}
