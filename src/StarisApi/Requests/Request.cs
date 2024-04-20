7f83e4c93152e90e4dae3ac0c5ff3c25a2ec249a
Alexandre Rodrigues <btt.ale@gmail.com>
2024-04-20 08:20:47 -0300

    feat: Configura filtros de pesquisa swagger

---
+++ src/StarisApi/Requests/Request.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarisApi.Models;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace StarisApi.Requests
+{
+    /// <summary>
+    /// testett
+    /// </summary>
+    public class Request
+    {
+        /// <summary>
+        /// Item de procura
+        /// </summary>
+        public string? Search { get; set; }
+        public int? Page { get; set; } = 1;
+        public int? PerPage { get; set; } = 10;
+        public string? SortBy { get; set; } = "id";
+        public string? SortOrder { get; set; } = "asc";
+        public int? Offset { get; set; } = 0;
+    }
+}
