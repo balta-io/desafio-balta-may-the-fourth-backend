0cb49706e5c1541803e8ec91c0c4bffcc14a50ed
leonardoGasperin <leonardovdg@hotmail.com>
2024-04-20 22:14:26 -0300

    criando entidade Planet sem as propiedades de FK

---
+++ src/StarisApi/Models/Planets/Planet.cs
@@ -0,0 +1,17 @@
+﻿namespace StarisApi.Models.Planets;
+
+public class Planet
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Diameter { get; set; } = null!;
+    public string RotationSpeed { get; set; } = null!;
+    public string OrbitalPeriod { get; set; } = null!;
+    public string Gravity { get; set; } = null!;
+    public string Population { get; set; } = null!;
+    public string Climate { get; set; } = null!;
+    public string Terrain { get; set; } = null!;
+    public string SurfaceWater { get; set; } = null!;
+    //public ICollection<CharacterPlanet> Characters { get; set; } = [];
+    //public ICollection<MoviePlanet> Movies { get; set; } = [];
+}
