b54428396f8e5a3a084624cbd7cc4df2cca6cced
leonardoGasperin <leonardovdg@hotmail.com>
2024-04-20 21:54:10 -0300

    criando entidade Movies sem propiedades de FK

---
+++ src/StarisApi/Models/Movies/Movies.cs
@@ -0,0 +1,16 @@
+﻿namespace StarisApi.Models.Movies;
+
+public class Movies
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public int Episode { get; set; }
+    public string OpeningCrawl { get; set; } = null!;
+    public string Director { get; set; } = null!;
+    public string Producer { get; set; } = null!;
+    public string ReleaseDate { get; set; } = null!;
+    //public ICollection<CharacterMovie> Characters { get; set; } = [];
+    //public ICollection<MoviePlanet> Planets { get; set; } = [];
+    //public ICollection<MovieVehicle> Vehicles { get; set; } = [];
+    //public ICollection<MovieStarship> Starships { get; set; } = [];
+}
