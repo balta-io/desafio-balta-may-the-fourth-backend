72e00cdfb616ff3c77af0563029107fa0f75781a
leonardoGasperin <leonardovdg@hotmail.com>
2024-04-20 22:07:08 -0300

    adicionando movies no db context

--- src/StarisApi/DbContexts/SqliteContext.cs
+++ src/StarisApi/DbContexts/SqliteContext.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using StarisApi.Models.Characters;
+using StarisApi.Models.Movies;
 
 namespace StarisApi.DbContexts
 {
     public class SqliteContext(DbContextOptions<SqliteContext> options) : DbContext(options)
     {
         public DbSet<Character> Characters { get; set; } = null!;
+        public DbSet<Movie> Movies { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CharacterDbMap());
+            modelBuilder.ApplyConfiguration(new MovieDbMap());
         }
     }
 }
