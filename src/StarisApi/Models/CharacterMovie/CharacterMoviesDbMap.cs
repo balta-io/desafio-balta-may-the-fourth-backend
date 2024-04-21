d4036084378b55c778fb98b8b40debda0ad9a46b
leonardoGasperin <leonardovdg@hotmail.com>
2024-04-21 16:14:09 -0300

    mapeamento da entidade relação personagem e filme

---
+++ src/StarisApi/Models/CharacterMovie/CharacterMoviesDbMap.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StarisApi.Models.CharacterMovie;
+
+public class CharacterMoviesDbMap : IEntityTypeConfiguration<CharacterMovie>
+{
+    public void Configure(EntityTypeBuilder<CharacterMovie> builder)
+    {
+        builder.ToTable("CharactersMoviesRelationship");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.CharacterId).HasColumnType("int").IsRequired();
+        builder.Property(x => x.MovieId).HasColumnType("int").IsRequired();
+    }
+}
