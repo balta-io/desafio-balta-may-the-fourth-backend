bcfc853e24c12399a0f995dbc50a3f48a1309a4b
leonardoGasperin <leonardovdg@hotmail.com>
2024-04-21 16:13:53 -0300

    cria entidade relação entre personagem e filme

---
+++ src/StarisApi/Models/CharacterMovie/CharacterMovie.cs
@@ -0,0 +1,10 @@
+﻿namespace StarisApi.Models.CharacterMovie;
+
+public class CharacterMovie
+{
+    public int Id { get; set; }
+    public int CharacterId { get; set; }
+    public int MovieId { get; set; }
+    //public Character Character { get; set; }
+    //public Movie Movie { get; set; }
+}
