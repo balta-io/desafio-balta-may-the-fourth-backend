e40a70e88aab990578490407527ae79dc6eb19a3
leonardoGasperin <leonardovdg@hotmail.com>
2024-04-20 21:34:24 -0300

    adicionando restante das propiedades mapeadas exceto FK

--- src/StarisApi/Models/Characters/Character.cs
+++ src/StarisApi/Models/Characters/Character.cs
@@ -1,8 +1,16 @@
-﻿namespace StarisApi.Models.Characters
+﻿namespace StarisApi.Models.Characters;
+
+public class Character
 {
-    public class Character
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string BirthYear { get; set; } = null!;
+    public string EyeColor { get; set; } = null!;
+    public string Gender { get; set; } = null!;
+    public string HairColor { get; set; } = null!;
+    public string Height { get; set; } = null!;
+    public string Mass { get; set; } = null!;
+    public string SkinColor { get; set; } = null!;
+    //public Planet? HomeWorld { get; set; }
+    //public ICollection<CharacterMovie> Movies { get; set; } = [];
 }
