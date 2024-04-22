7737a665c28aad050ed76afff2fc7355d8db2b9a
Alexandre Rodrigues <btt.ale@gmail.com>
2024-04-22 11:23:39 -0300

    feat: implementa repository e configura dtos e entidade base

--- src/StarisApi/Models/Characters/Character.cs
+++ src/StarisApi/Models/Characters/Character.cs
@@ -1,8 +1,25 @@
-﻿namespace StarisApi.Models.Characters
+﻿using StarisApi.Dtos;
+using System.Globalization;
+using System.Reflection;
+
+namespace StarisApi.Models.Characters
 {
-    public class Character
+    public sealed class Character : Entity
     {
-        public int Id { get; set; }
         public string Name { get; set; } = null!;
+
+        public override T ConvertToDto<T>()
+        {
+            var character = new CharacterDto
+                            {
+                                Id = Id,
+                                Name = Name
+                            
+                            } as T;
+
+            return character!;
+        }
+
+        public override string GetSearchParameter() => "Name";
     }
 }
