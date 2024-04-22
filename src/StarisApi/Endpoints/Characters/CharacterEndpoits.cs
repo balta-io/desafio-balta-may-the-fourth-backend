683698b0da453c5d2f42e038ac3d85baa4bfdd44
Alexandre Rodrigues <btt.ale@gmail.com>
2024-04-22 11:24:01 -0300

    feat: cria segundo endpoint exemplo

--- src/StarisApi/Endpoints/Characters/CharacterEndpoits.cs
+++ src/StarisApi/Endpoints/Characters/CharacterEndpoits.cs
@@ -1,5 +1,10 @@
-﻿using StarisApi.DbContexts;
+﻿using Microsoft.AspNetCore.Mvc;
+using StarisApi.DbContexts;
+using StarisApi.Dtos;
+using StarisApi.Models.Characters;
+using StarisApi.Repository;
 using StarisApi.Requests;
+using StarisApi.Responses;
 
 namespace StarisApi.Endpoints.Characters
 {
@@ -7,11 +12,23 @@ namespace StarisApi.Endpoints.Characters
     {
         public static IEndpointRouteBuilder MapCharacterEndpoits(this IEndpointRouteBuilder app)
         {
-            
-            app.MapGet("/character", ([AsParameters] Request request, SqliteContext context) => 
+
+            app.MapGet("/character", ([AsParameters] Request request, Repository<Character> context) =>
             {
-                var chracters = context.Characters.ToList();
-                return Results.Ok(chracters);
+                var characters = context.GetAll<CharacterDto>(request);
+                return characters.Results.Any() ? TypedResults.Ok(characters) : Results.NoContent();
+
+            }).WithTags("Character")
+              .Produces(TypedResults.Ok().StatusCode)
+              .Produces(TypedResults.NoContent().StatusCode)
+              .WithOpenApi();
+
+            app.MapGet("/character/{id}", ([FromRoute] int id, Repository<Character> context) =>
+            {
+                var character = context.Find(id);
+
+                return character is null ? Results.NotFound() : TypedResults.Ok(character);
+
             }).WithTags("Character")
               .WithOpenApi();
 
