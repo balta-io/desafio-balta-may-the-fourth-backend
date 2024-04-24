201dd1eac0b4ffa1b3030982432d522a815e381a
Fabricio <fabriciosilvalp@outlook.com>
2024-04-23 22:52:06 -0300

    SQL script to create Character Table on the Database

---
+++ StarWars.API/Storages/Datas/charactertable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE characters (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            height VARCHAR(255) DEFAULT NULL,
+                            mass VARCHAR(255) DEFAULT NULL,
+                            hair_color VARCHAR(255) DEFAULT NULL,
+                            skin_color VARCHAR(255) DEFAULT NULL,
+                            eye_color VARCHAR(255) DEFAULT NULL,
+                            birth_year VARCHAR(255) DEFAULT NULL,
+                            gender VARCHAR(255) DEFAULT NULL,
+                            name VARCHAR(255) DEFAULT NULL,
+                            homeworld VARCHAR(255) DEFAULT NULL
+);
