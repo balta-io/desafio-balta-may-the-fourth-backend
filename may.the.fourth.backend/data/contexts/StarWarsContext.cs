efcd9de389d2745375a120ceee328f693bfd8cf0
Paulo Pimenta <pimenta_global@hotmail.com>
2024-04-24 12:36:48 -0400

    creating the database in postgres

---
+++ may.the.fourth.backend/data/contexts/StarWarsContext.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+
+using May.The.Fourth.Backend.Data.Entities;
+
+namespace May.The.Fourth.Backend.Data.Contexts
+{
+    public class StarWarsContext : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public StarWarsContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("StarWarsConnection"));
+        }
+
+        public DbSet<FilmeEntity> Filmes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // filmes
+            var filmes = new FilmeEntity[]
+            {
+                new FilmeEntity { Id = 1, Titulo = "The Rise of the Jedi", Episodio = 10, TextoAbertura = "After the fall of the Empire, the galaxy face", Diretor = "Jana Doe", Produtor = "Leo Smith", DataLancamento = new DateTime(2028, 12, 15) },
+                new FilmeEntity { Id = 2, Titulo = "The Battle of the Stars" },
+                new FilmeEntity { Id = 3, Titulo = "Return of the Light" },
+                new FilmeEntity { Id = 4, Titulo = "Warriors of the Shadow Realm" },
+                new FilmeEntity { Id = 5, Titulo = "The Galactic Quest" },
+                new FilmeEntity { Id = 6, Titulo = "Rise of the Planetara" },
+                new FilmeEntity { Id = 7, Titulo = "Echoes of the Stars" },
+                new FilmeEntity { Id = 8, Titulo = "The Return of the Voyager" },
+                new FilmeEntity { Id = 9, Titulo = "Voyager''s Endgame" },
+                new FilmeEntity { Id = 10, Titulo = "Galactic Odyssey" },
+                new FilmeEntity { Id = 11, Titulo = "The Edge of the Universe" },
+            };
+            modelBuilder.Entity<FilmeEntity>().HasData(filmes);
+        }
+    }
+}
