e7a5612750397eefd8cd54090d1c290f83285e51
Everson Alves de Rezende <erezende@osg.com.br>
2024-04-25 10:10:58 -0300

    Add initial swagger configuration.

--- src/MayTheFourth.Api/Program.cs
+++ src/MayTheFourth.Api/Program.cs
@@ -5,6 +5,8 @@ using MayTheFourth.Api.Extensions.Contexts.PersonContext;
 using MayTheFourth.Api.Extensions.Contexts.PlanetContext;
 using MayTheFourth.Api.Extensions.Contexts.StartshipContext;
 using Microsoft.AspNetCore.Http.Json;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,7 +24,30 @@ builder.Services.Configure<JsonOptions>(opt => opt.SerializerOptions.ReferenceHa
 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(api =>
+{
+    api.SwaggerDoc("v1", new()
+    {
+        Title = "MayTheFourth - MT4BWY",
+        Version = "v1",
+        Description = "API para consulta de dados do universo de Star Wars",
+        TermsOfService = new Uri("https://go.balta.io/may-the-fourth"),
+        Contact = new OpenApiContact
+        {
+            Name = "Capitão Igor",
+            Url = new Uri("https://github.com/igorsantiiago/desafio-balta-may-the-fourth-backend")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License"
+        }
+    });
+
+    api.CustomSchemaIds(type => type.FullName);
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    api.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+});
 
 var app = builder.Build();
 
