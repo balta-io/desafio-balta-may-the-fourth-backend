c898f4b03dba8760825a982255e1e9c8bacc2ce7
marco <marcogs713@gmail.com>
2024-04-27 17:36:09 -0300

    ajustado os atributos da model e do sql

--- StarWars.API/Models/VehicleModel.cs
+++ StarWars.API/Models/VehicleModel.cs
@@ -2,17 +2,18 @@ namespace StarWars.API.Models;
 
 public class VehicleModel
 {
-    public int VehicleId { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Model { get; set; }
     public string Manufacturer { get; set; }
-    public int? CostInCredits { get; set; }
+    public string? CostInCredits { get; set; }
     public string Length { get; set; }
     public string MaxSpeed { get; set; }
-    public int? Crew { get; set; }
-    public int? Passengers { get; set; }
+    public string? Crew { get; set; }
+    public string? Passengers { get; set; }
     public string CargoCapacity { get; set; }
     public string Consumables { get; set; }
     public string Class { get; set; }
     public List<MovieModel> Movies { get; set; }
+    public List<CharacterModel> Character { get; set; }
 }
--- StarWars.API/Storages/Datas/SQLScripts/vehicles.sql
+++ StarWars.API/Storages/Datas/SQLScripts/vehicles.sql
@@ -1,14 +1,16 @@
 CREATE TABLE vehicles (
                           id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                           name VARCHAR(255) NOT NULL,
-                          model VARCHAR(255)DEFAULT NULL,
-                          manufacturer VARCHAR(255) DEFAULT NULL,
-                          costInCredits INT DEFAULT NULL,
-                          length VARCHAR(50)DEFAULT NULL,
-                          maxSpeed VARCHAR(50)DEFAULT NULL,
-                          crew INT DEFAULT NULL,
-                          passengers INT DEFAULT NULL,
-                          cargoCapacity VARCHAR(50) DEFAULT NULL,
-                          consumables VARCHAR(50) DEFAULT NULL,
-                          class VARCHAR(50) DEFAULT NULL
+                          model VARCHAR(255),
+                          vehicle_class VARCHAR(255),
+                          manufacturer VARCHAR(255),
+                          cost_in_credits VARCHAR(255),
+                          length VARCHAR(255),
+                          crew VARCHAR(255),
+                          passengers VARCHAR(255),
+                          max_atmosphering_speed VARCHAR(255),
+                          cargo_capacity VARCHAR(255),
+                          consumables VARCHAR(255),
+                          movieId INT DEFAULT NULL,
+                          characterId INT DEFAULT NULL
 );
